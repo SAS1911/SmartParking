3dbb7546eac6c043f59b33bcf7345460c476beb8
filippsalov <168278961+filippsalov@users.noreply.github.com>
2024-04-29 19:25:13 +0200

    Add files via upload

--- src/modelo/gestoresplazas/GestorLocalidad.java
+++ src/modelo/gestoresplazas/GestorLocalidad.java
@@ -7,48 +7,42 @@ import modelo.reservas.solicitudesreservas.SolicitudReservaAnticipada;
 //TO-DO alumno obligatorio
 
 public class GestorLocalidad {
+	
 	private GestorZona[][] gestoresZonas;
 
-
+	//CONSTRUCTOR
 	public GestorLocalidad(int[][] plazas, double[][] precios) {
-		//HECHO?
 		gestoresZonas = new GestorZona[precios.length][precios[0].length];//Inicializamos el tamaño 
 		for(int i=0; i<precios.length; i++) {
 			for(int j=0; j<precios[i].length; j++) {
-				gestoresZonas[i][j] = new GestorZona (i, j, plazas[i][j], precios[i][j]);//Recorremos el array bidimensional e inicializamos cada zona
+				gestoresZonas[i][j] = new GestorZona (i, j, plazas[i][j], precios[i][j]);//Recorremos el array bidimensional e inicializamos cada GestorZona
 			}
 		}
 	}
 	
 	public int getRadioMaxI() {
-		//HECHO?
 		return gestoresZonas.length-1;
 	}
 	
 	public int getRadioMaxJ() {
-		//HECHO?
 		return gestoresZonas[0].length-1; 
 	}
 	
 	public boolean existeZona(int i, int j) {
-		//HECHO?
 		return 0<=i && i<=getRadioMaxI() && 0<=j && j<=getRadioMaxJ(); //Debe ser mayor o igual que 0 y menor o igual que el radio máximo
 	}
 
 	public boolean existeHuecoReservado(Hueco hueco, int i, int j) {
-		//HECHO?
 		return gestoresZonas[i][j].existeHuecoReservado(hueco);
 	}
 
 	public GestorZona getGestorZona(int i, int j) {
-		//HECHO?
 		return gestoresZonas[i][j];
 	}
 	
 	//TO-DO alumno opcional
 	
 	public IList<SolicitudReservaAnticipada> getSolicitudesAtendidasListaEspera(int i, int j) {
-		//HECHO?
 		return gestoresZonas[i][j].getSolicitudesAtendidasListaEspera();//Se realiza la opreación en la clase hija GestorZona
 	}
 	
--- src/modelo/gestoresplazas/GestorZona.java
+++ src/modelo/gestoresplazas/GestorZona.java
@@ -12,6 +12,7 @@ import modelo.gestoresplazas.huecos.Plaza;
 import modelo.reservas.solicitudesreservas.SolicitudReservaAnticipada;
 
 public class GestorZona {
+	
 	private int iZona;
 	private int jZona;
 	private Plaza[] plazas;
@@ -19,6 +20,7 @@ public class GestorZona {
 	private IList<SolicitudReservaAnticipada> listaEspera;
 	private GestorHuecos gestorHuecos;
 	private IList<Hueco> huecosReservados;
+	
 	
 	public int getI() {
 		return iZona;
@@ -55,21 +57,16 @@ public class GestorZona {
 	public String toString() {
 		return getId() + ": " + getEstadoHuecosReservados();
 	}
-
-	public boolean existeHueco(LocalDateTime tI, LocalDateTime tF) {
-		return gestorHuecos.existeHueco(tI, tF);
-	}
 	
 	//TO-DO alumno obligatorios
 	
+	//CONSTRUCTOR
 	public GestorZona(int i, int j, int noPlazas, double precio){
-		//HECHO?
 		iZona = i;
 		jZona = j;
-		plazas = new Plaza[noPlazas];//Inicializamos el vector de plazas
-		for(int k=0; k<plazas.length; k++) {//Y a cada plaza le damos su número
+		plazas = new Plaza[noPlazas];//Inicializamos el vector de plazas con el número de plazas indicado
+		for(int k=0; k<plazas.length; k++) //Y a cada plaza le damos su número
 			plazas[k] = new Plaza(k);
-		}
 		this.precio = precio;
 		listaEspera = new ArrayList<SolicitudReservaAnticipada>();//Inicializamos la lista de espera como ArrayList
 		huecosReservados =  new ArrayList<Hueco>();//Al igual que huecos reservados
@@ -77,25 +74,22 @@ public class GestorZona {
 	}
 	
 	public Hueco reservarHueco(LocalDateTime tI, LocalDateTime tF) {
-		//HECHO?
 		//Creamos la variable hueco, no hace falta comprobar si existe ya que ya lo hace el método reservarHueco(...)
-		Hueco hueco = gestorHuecos.reservarHueco(tI, tF);//Creamos el hueco que vamos a reservar, será null si no es posible
+		Hueco hueco = gestorHuecos.reservarHueco(tI, tF);//Seleccionamos el hueco que vamos a reservar, será null si no es posible
 		if(hueco!=null)//En caso de existir:
 			huecosReservados.add(huecosReservados.size(), hueco);//Lo añadimos a la lista de huecos reservados
 		return hueco;
 	}
 	
 	public void meterEnListaEspera(SolicitudReservaAnticipada solicitud) {//Método implementado en SolicitudReservaAnticipada
-		//HECHO?
 		listaEspera.add(listaEspera.size(), solicitud);//Si una solicitud no es posible admitirla, la metemos en la lista de espera
 		
 	}
 	
 	public boolean existeHuecoReservado(Hueco hueco) {
-		//HECHO? 
 		boolean existe = false;
 			for(int i=0; i<huecosReservados.size() && !existe; i++) {//Recorremos el array
-	//			if(huecosReservados.get(i).equals(hueco)) existe = true;//El equals parece que está sobreescrito incorrectamente
+//				if(huecosReservados.get(i).equals(hueco)) existe = true;//El equals parece que está sobreescrito incorrectamente, da error en el JUnit
 				if(huecosReservados.get(i).getPlaza() == hueco.getPlaza() &&//Comparamos la plaza
 						huecosReservados.get(i).gettF() == hueco.gettF() &&//El tiempo final
 						huecosReservados.get(i).gettI() == hueco.gettI()) existe = true;//El tiempo inicial
@@ -103,34 +97,46 @@ public class GestorZona {
 		return existe;
 	}
 	
+	public boolean existeHueco(LocalDateTime tI, LocalDateTime tF) {
+		return gestorHuecos.existeHueco(tI, tF);
+		}
 	
 	//TO-DO alumno opcionales
 	
 	public void liberarHueco(Hueco hueco) {
-		//HECHO?
-		if(hueco!=null) {//En caso de no ser nulo
-			gestorHuecos.liberarHueco(hueco);//Liberamos el hueco
-			huecosReservados.remove(hueco);//Lo quitamos del array
-		}
+		gestorHuecos.liberarHueco(hueco);//Liberamos el hueco
+		huecosReservados.remove(hueco);//Lo quitamos del array
 	}
 
 	//PRE (no es necesario comprobar): las solicitudes de la lista de espera son válidas
 	public IList<SolicitudReservaAnticipada> getSolicitudesAtendidasListaEspera() {
-		//HECHO?
-		Hueco hueco = null;
-		IList<SolicitudReservaAnticipada> solicitudesSerAtendidas = new ArrayList<>();
-		for (int i=0; i<listaEspera.size(); i++) {
-			hueco = listaEspera.get(i).getHueco();
-			if(hueco!=null && !existeHuecoReservado(hueco)) {
-				solicitudesSerAtendidas.add(solicitudesSerAtendidas.size(), listaEspera.get(i));
+		SolicitudReservaAnticipada solicitud;
+		Hueco huecoReserva;
+		
+		IList<SolicitudReservaAnticipada> solicitudesSerAtendidas = new ArrayList<>();//Creamos la lista a devolver
+		
+		for (int i=0; i<listaEspera.size(); i++) {//La recorremos mediante un for
+			solicitud = listaEspera.get(i);
+			huecoReserva = gestorHuecos.reservarHueco(solicitud.getTInicial(), solicitud.getTFinal());//Vemos si el hueco de la solicitud está disponible
+			if(huecoReserva!=null) {//En caso de estar disponible
+				solicitud.setHueco(huecoReserva);//Cambiamos el hueco de la solicitud al obtenido
+				/*
+				 * IMPORTANTE
+				 * El setter anterior lo empleamos ya que posteriormente, en la clase de ControladoresReservas,
+				 * método getReservasRegistradasDesdeListaEspera(int i, int j), se emplea el método hacerReserva(SolicitudReserva solicitud),
+				 * ahora bien, este ser realizará correctamente si el hueco de la solicitud está liberado, entonces, lo que hacemos es,
+				 * arrastrar este hueco que hemos reservado con la solicitud y posteriormente lo liberamos otra vez para poder hacer
+				 * la reserva correctamente. Esta liberación se hace una vez asignados todos las solicitudes que pueden ser atendidas
+				 * para no crear bucles indefinidos.
+				 */
+				solicitudesSerAtendidas.add(solicitudesSerAtendidas.size(), solicitud);//Y añadimos la solicitud a la lista
 			}
 		}
-		for (int j=0; j<solicitudesSerAtendidas.size();j++) 
-			listaEspera.remove(solicitudesSerAtendidas.get(j));
+		for (int j=0; j<solicitudesSerAtendidas.size();j++) { //Y al final todo lo que pueda ser atendido de la lista de espera
+			solicitud = solicitudesSerAtendidas.get(j);
+			listaEspera.remove(solicitud);
+			gestorHuecos.liberarHueco(solicitud.getHueco());//Liberamos el hueco según lo previamente explicado
+		}
 		return solicitudesSerAtendidas;
 	}
-
-	
-
-
 }
